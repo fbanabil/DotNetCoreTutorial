aa25ac4a4a7b117ea33971012d66ebda1d272267
fbanabil <fahadbinaziznabil@gmail.com>
2025-07-17 23:43:51 +0600

    Advancement

--- DotnetCoreTutorial/DotnetCoreTutorial/Program.cs
+++ DotnetCoreTutorial/DotnetCoreTutorial/Program.cs
@@ -1,6 +1,24 @@
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.Run(async (HttpContext context) =>
+{
+    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+
+    if (context.Request.Method == "GET")
+    {
+        if(context.Request.Query.ContainsKey("id"))
+        {
+           string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    }
+    
+});
+
+
+
 
 app.Run();
