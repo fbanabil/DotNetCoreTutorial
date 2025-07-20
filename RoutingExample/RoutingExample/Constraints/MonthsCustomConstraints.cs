2f957659e9a5d579f768a485d75e9ae3eb5503eb
fbanabil <fahadbinaziznabil@gmail.com>
2025-07-20 17:40:10 +0600

    Regex costraints,custom constraints in parameters

---
+++ RoutingExample/RoutingExample/Constraints/MonthsCustomConstraints.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace RoutingExample.Constraints
+{
+    public class MonthsCustomConstraints : IRouteConstraint
+    {
+        public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            if(!values.ContainsKey(routeKey))
+            {
+                return false;
+            }
+            
+            Regex regex = new Regex("^(apr|jul|oct|jan)$", RegexOptions.IgnoreCase);
+            string? montValue = values[routeKey]?.ToString();   
+
+            if(regex.IsMatch(montValue ?? string.Empty))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- RoutingExample/RoutingExample/Program.cs
+++ RoutingExample/RoutingExample/Program.cs
@@ -1,4 +1,14 @@
+using RoutingExample.Constraints;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddRouting(options =>
+{
+    options.ConstraintMap.Add("months",typeof(MonthsCustomConstraints));
+});
+
+
 var app = builder.Build();
 
 
@@ -77,13 +87,104 @@ app.UseEndpoints(endpoints =>
         await context.Response.WriteAsync("In Files");
     });
 
-    // Optional Routing Parameters
 
+    // Optional Routing Parameters
     endpoints.Map("products/details/{id?}", async context =>
     {
-        int id=Convert.ToInt32(context.Request.RouteValues["id"]);
-        await context.Response.WriteAsync($"Product ID: {id}\n");
+        if(context.Request.RouteValues.ContainsKey("id"))
+        {
+            int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+            await context.Response.WriteAsync($"Product ID: {id}\n");
+        }
+        else
+        {
+            await context.Response.WriteAsync("Product ID not provided.\n");
+        }
     });
+
+    //Route constraints
+    endpoints.Map("products/{id:int?}", async context =>
+    {
+        int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+        await context.Response.WriteAsync($"Product ID (int): {id}\n");
+    });
+
+    // Route constraints with date parameter
+    endpoints.Map("products/date/{date:datetime}", async context =>
+    {
+        if (context.Request.RouteValues.ContainsKey("date"))
+        {
+            DateTime date = Convert.ToDateTime(context.Request.RouteValues["date"]);
+            await context.Response.WriteAsync($"Product Date: {date.ToShortDateString()}\n");
+        }
+        else
+        {
+            await context.Response.WriteAsync("Product Date not provided.\n");
+        }
+    });
+
+    // Guid id as route parameter
+    endpoints.Map("products/guid/{id:guid}", async context =>
+    {
+        if (context.Request.RouteValues.ContainsKey("id"))
+        {
+            Guid id = Guid.Parse(Convert.ToString(context.Request.RouteValues["id"])!);
+            await context.Response.WriteAsync($"Product GUID: {id}\n");
+        }
+        else
+        {
+            await context.Response.WriteAsync("Product GUID not provided.\n");
+        }
+    });
+
+    // minimum length constraint
+    endpoints.Map("products/minlength/{name:minlength(3):maxlength(10)=Clock}", async context =>
+    {
+        // minlength(3):maxlength(10) = length(4,7)
+        string name = Convert.ToString(context.Request.RouteValues["name"])!;
+        await context.Response.WriteAsync($"Product Name: {name}\n");
+    });
+
+    //min max in int constraint
+    endpoints.Map("products/minmax/{id:int:min(1):max(100)=10}", async context =>
+    {
+        // min(1):max(100) = range(1,100)
+        int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+        await context.Response.WriteAsync($"Product ID (Min 1, Max 100): {id}\n");
+    });
+
+    // Range in int constraint
+    endpoints.Map("products/range/{id:int:range(1,100)}", async context =>
+    {
+        int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+        await context.Response.WriteAsync($"Product ID (Range 1-100): {id}\n");
+    });
+
+    // Only alphabate
+    endpoints.Map("products/alpha/{name:alpha=clock}", async context =>
+    {
+        string name = Convert.ToString(context.Request.RouteValues["name"])!;
+        await context.Response.WriteAsync($"Product Name (Alpha): {name}\n");
+    });
+
+    // Regex (Regular Expression) as route constraint
+    endpoints.Map("sales-report/{year:int:min(1900)}/" +
+        "{month:regex(^(apr|jul|oct|jan)$)}", async context =>
+    {
+        int year=Convert.ToInt32(context.Request.RouteValues["year"]);
+        string month = Convert.ToString(context.Request.RouteValues["month"])!;
+        await context.Response.WriteAsync($"Sales Report for {month}-{year}\n");
+    });
+
+    // Use of custom constraint using matching
+    endpoints.Map("sales-report-check/{year:int:min(1900)}/" +
+        "{month:months}", async context =>
+        {
+            int year = Convert.ToInt32(context.Request.RouteValues["year"]);
+            string month = Convert.ToString(context.Request.RouteValues["month"])!;
+            await context.Response.WriteAsync($"Sales Report for {month}-{year}\n");
+        });
+
 });
 
 
