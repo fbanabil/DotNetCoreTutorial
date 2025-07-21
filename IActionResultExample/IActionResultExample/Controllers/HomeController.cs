2f3e6a509a4a54473eeb1e194ff403421d3bfa18
fbanabil <fahadbinaziznabil@gmail.com>
2025-07-22 01:22:28 +0600

    IActionResult, Redirection

---
+++ IActionResultExample/IActionResultExample/Controllers/HomeController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IActionResultExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [Route("book")]
+        public IActionResult Index()
+        {
+            // Check if the query string contains a specific key
+            if (!Request.Query.ContainsKey("bookid")) 
+            {
+                //Response.StatusCode = 400; // Bad Request
+                //return Content("No book ID provided.");
+                
+                // Shortcuts
+                return BadRequest("No book ID Provided");
+            }
+            // Book can not nbe empty
+            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
+            {
+                //Response.StatusCode = 400; // Bad Request
+                //return Content("Book ID cannot be empty.");
+                
+                // Shortcuts
+                return BadRequest("Book ID cannot be empty.");
+
+            }
+
+            // Book ID should be betwwen 1 to 1000
+            int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookid"]);
+            //_logger.LogInformation($"Book ID: {bookId}");
+            if (bookId < 1 || bookId > 1000) 
+            {
+                //Response.StatusCode = 404; // Not Found
+                //return Content("Book ID must be between 1 and 1000.");
+                
+                // Shortcuts
+                return NotFound("Book ID must be between 1 and 1000.");
+            }
+
+            // Checking IsLoggedIn
+
+            if (Convert.ToBoolean(Request.Query["IsLoggedIn"]) == false)
+            {
+                //Response.StatusCode = 401; // Unauthorized
+                //return Content("You must be logged in to access this resource.");
+                
+                // Shortcuts
+                return Unauthorized("You must be logged in to access this resource.");
+                //return StatusCode(401);
+            }
+
+            return File("/sample.pdf", "application/pdf");
+        }
+
+
+        [Route("bookstore")]
+        public IActionResult BookStore()
+        {
+
+            if (!Request.Query.ContainsKey("bookid"))
+            {
+                //Response.StatusCode = 400; // Bad Request
+                //return Content("No book ID provided.");
+
+                // Shortcuts
+                return BadRequest("No book ID Provided");
+            }
+            // Book can not nbe empty
+            if (string.IsNullOrEmpty(Convert.ToString(Request.Query["bookid"])))
+            {
+                //Response.StatusCode = 400; // Bad Request
+                //return Content("Book ID cannot be empty.");
+
+                // Shortcuts
+                return BadRequest("Book ID cannot be empty.");
+
+            }
+
+            // Book ID should be betwwen 1 to 1000
+            int bookId = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookid"]);
+           // _logger.LogInformation($"Book ID: {bookId}");
+            if (bookId < 1 || bookId > 1000)
+            {
+                //Response.StatusCode = 404; // Not Found
+                //return Content("Book ID must be between 1 and 1000.");
+
+                // Shortcuts
+                return NotFound("Book ID must be between 1 and 1000.");
+            }
+
+            // Checking IsLoggedIn
+
+            if (Convert.ToBoolean(Request.Query["IsLoggedIn"]) == false)
+            {
+                //Response.StatusCode = 401; // Unauthorized
+                //return Content("You must be logged in to access this resource.");
+
+                // Shortcuts
+                return Unauthorized("You must be logged in to access this resource.");
+                //return StatusCode(401);
+            }
+
+            //return new RedirectToActionResult("Books", "Store", new {});//302 - Found
+
+            // Shortcut
+            //return RedirectToAction("Books","Store", new { id = bookId}); //302 - Found : Search engine will not store the new url
+
+
+            // Or
+
+            //return new RedirectToActionResult("Books", "Store", new { }, true); //301 - moved permenently : Search engine will store the new url
+
+            // shortcut
+
+            //return RedirectToActionPermanent("Books", "Store", new { id = bookId }); //301 - moved permenently : Search engine will store the new url
+
+
+            // Local Redirect
+            return new LocalRedirectResult("/store/books?id=" + bookId);
+            return LocalRedirect("/store/books?id=" + bookId); //302 - Found : Search engine will not store the new url
+
+        }
+    }
+}
---
+++ IActionResultExample/IActionResultExample/Controllers/StoreController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionResultExample.Controllers
+{
+    public class StoreController : Controller
+    {
+        [Route("store/books")]
+        public IActionResult Books()
+        {
+            int id= Convert.ToInt32(Request.Query["id"]);
+            return Content($"<h1>Books List</h1><p>Here are some books available in our store. Book Id {id}</p>","text/html");
+        }
+    }
+}
