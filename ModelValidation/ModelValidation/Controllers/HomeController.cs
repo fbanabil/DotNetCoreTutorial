5f5bc97081cf10d37adc9c273c5377048765ed32
fbanabil <fahadbinaziznabil@gmail.com>
2025-07-23 21:35:18 +0600

    Bind and Bindnever

--- ModelValidation/ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/ModelValidation/Controllers/HomeController.cs
@@ -37,5 +37,41 @@ namespace ModelValidation.Controllers
             }
             return Content($"{person}");
         }
+
+
+
+        [Route("[action]")]
+        public IActionResult BindTest([Bind(nameof(Person.PersonName),nameof(Person.Email)
+            ,nameof(Person.Password),nameof(Person.ConfirmPassword))]Person person)//Only bind the specified properties
+        {
+            if (!ModelState.IsValid)
+            {
+                //List<string> errorsList = new List<string>();
+                //foreach (var value in ModelState.Values)
+                //{
+                //    foreach(var error in value.Errors)
+                //    {
+                //        errorsList.Add(error.ErrorMessage);
+                //    }
+                //}
+                //string errors = string.Join("\n", errorsList);
+
+                // or
+
+                //List<string> errorsList = ModelState.Values.SelectMany(values =>
+                //values.Errors.Select(err => err.ErrorMessage)).ToList();
+                //string errors = string.Join("\n", errorsList);
+
+                //or
+                List<string> errorsList = ModelState.Values.SelectMany(value =>
+                value.Errors).Select(err => err.ErrorMessage).ToList();
+                string errors = string.Join("\n", errorsList);
+                return BadRequest(errors);
+
+                // If the model state is invalid, return the validation errors
+                //return BadRequest(ModelState);
+            }
+            return Content($"{person}");
+        }
     }
 }
