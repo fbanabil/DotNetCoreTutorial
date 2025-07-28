c25f2667b9d127db0bea414ad9d39cc4bb27e690
fbanabil <fahadbinaziznabil@gmail.com>
2025-07-28 22:35:46 +0600

    Viwe in mvc, code blocks, if, switch, foreach, for, literal,local function, html.raw, viewdata

---
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewsExample.Models;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            string title = "Asp.Net Core App";
+
+            List<Person> people = new List<Person>
+            {
+                new Person
+                {
+                    Name = "Fba Nabil",
+                    DateOfBirth = Convert.ToDateTime("2002-10-04"),
+                    PersonGender = Gender.Male
+                },
+                new Person
+                {
+                    Name = "Denesh Barua",
+                    DateOfBirth = Convert.ToDateTime("2002-06-19"),
+                    PersonGender = Gender.Other
+                },
+                new Person
+                {
+                    Name = "Atfan Bin Nur",
+                    DateOfBirth = Convert.ToDateTime("2000-03-24"),
+                    PersonGender = Gender.Male
+                }
+            };
+
+
+
+            ViewData["title"]= title;
+            ViewData["people"] = people;
+            return View(); // Views/Home/Index.cshtml
+            // o
+            return new ViewResult { ViewName = "Index" };
+
+        }
+    }
+}
