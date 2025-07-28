48adcaeebeae65ec673cd2fa6afa63f32310b1fb
fbanabil <fahadbinaziznabil@gmail.com>
2025-07-29 01:32:42 +0600

    Viewdata, viewbag, strongly tped views, _viewimports, sharedviews

--- ViewsExample/ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/ViewsExample/Controllers/HomeController.cs
@@ -28,19 +28,93 @@ namespace ViewsExample.Controllers
                 new Person
                 {
                     Name = "Atfan Bin Nur",
-                    DateOfBirth = Convert.ToDateTime("2000-03-24"),
+                    DateOfBirth = null,
                     PersonGender = Gender.Male
                 }
             };
 
 
 
-            ViewData["title"]= title;
+            //ViewData["title"]= title;  
+
+            //ViewBag.title = title;
             ViewData["people"] = people;
-            return View(); // Views/Home/Index.cshtml
+            //ViewBag.people = people;
+            return View("Index",people); // Views/Home/Index.cshtml   // @model using argument
             // o
             return new ViewResult { ViewName = "Index" };
 
+        }
+
+        [Route("person-details/{name}")]
+        public IActionResult Details(string? name)
+        {
+
+            if(name==null)
+            {
+                return Content("Person name can't be null");
+            }
+
+            List<Person> people = new List<Person>
+            {
+                new Person
+                {
+                    Name = "Fba Nabil",
+                    DateOfBirth = Convert.ToDateTime("2002-10-04"),
+                    PersonGender = Gender.Male
+                },
+                new Person
+                {
+                    Name = "Denesh Barua",
+                    DateOfBirth = Convert.ToDateTime("2002-06-19"),
+                    PersonGender = Gender.Other
+                },
+                new Person
+                {
+                    Name = "Atfan Bin Nur",
+                    DateOfBirth = null,
+                    PersonGender = Gender.Male
+                }
+            };
+
+            Person? matchingPerson = people.Where(temp=> temp.Name?.ToLower() == name?.ToLower()).FirstOrDefault();
+
+            return View(matchingPerson);
+        }
+
+
+        [Route("person-with-product")]
+        public IActionResult PersonWithProduct()
+        {
+            Person person = new Person
+            {
+                Name = "Fba Nabil",
+                DateOfBirth = Convert.ToDateTime("2002-10-04"),
+                PersonGender = Gender.Male
+            };
+
+            Product product = new Product
+            {
+                ProductId = 1,
+                ProductName = "Laptop"
+            };
+
+            PersonAndProductWrapperModel model = new PersonAndProductWrapperModel
+            {
+                PersonData = person,
+                ProductData = product
+            };
+
+            return View(model);
+        }
+
+
+        [Route("home/all-products")]
+        public IActionResult All()
+        {
+            //Views/Products.All.cshtml
+            // Views/Shared/All.cshtml
+            return View();
         }
     }
 }
---
+++ ViewsExample/ViewsExample/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [Route("products/all")]
+        public IActionResult All()
+        {
+            //Views/Products.All.cshtml
+            // Views/Shared/All.cshtml
+            return View();
+        }
+    }
+}
