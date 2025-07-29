85b54c3709df675092efa6247d8eb34daa53df07
fbanabil <fahadbinaziznabil@gmail.com>
2025-07-29 22:38:15 +0600

    Layout and nested layout

---
+++ LayoutViewsExample/LayoutViewsExample/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LayoutViewsExample.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("products")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("search-products/{ProductId?}")]
+        public IActionResult Search(int? ProductId)
+        {
+            ViewBag.ProductID = ProductId;
+            return View();
+        }
+
+        [Route("order-products")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
