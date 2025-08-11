003e07ae6d788e63d0968242207d34262507e6df
fbanabil <fahadbinaziznabil@gmail.com>
2025-08-11 22:33:58 +0600

    Configuration(Hierarchical, Option pattern, As Servise, Enviroment)

---
+++ ConfigurationExample/ConfigurationExample/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using ConfigurationExample.OptionModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly WeatherApiOptions _options;
+
+        public HomeController(IConfiguration configuration,IOptions<WeatherApiOptions> options)
+        {
+            _configuration = configuration;
+            _options = options.Value;
+        }
+
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            // Accessing configuration values
+            ViewBag.MyKey = _configuration["MyKey"];
+            ViewBag.MyApiKey = _configuration.GetValue<string>("MyApiKey","The Default Key");
+            ViewBag.ClientId = _configuration["weatherapi:ClientId"];
+            ViewBag.ClientSecret = _configuration.GetValue("weatherapi:ClientSecret","Default");
+
+            // Accessing a configuration section
+            ViewBag.SectionClient = _configuration.GetSection("weatherapi")["ClientId"];
+
+            // Accessing a configuration section as a strongly typed object
+            WeatherApiOptions? options = _configuration.GetSection("weatherapi").Get<WeatherApiOptions>();
+            ViewBag.OptionsClientId = options?.ClientID;
+            ViewBag.OptionsClientSecret = options?.ClientSecret;
+
+            // Binding a configuration section to a strongly typed object
+            WeatherApiOptions weatherApiOptions = new WeatherApiOptions();
+            _configuration.GetSection("weatherapi").Bind(weatherApiOptions);
+            ViewBag.BoundClientId = weatherApiOptions.ClientID;
+
+            // Accessing options via IOptions
+            ViewBag.IOptionsClientId = _options.ClientID;
+
+            return View();
+        }
+    }
+}
