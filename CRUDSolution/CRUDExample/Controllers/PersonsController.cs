ced63d7b11ecb01c826d4956cf82882e587266a6
fbanabil <fahadbinaziznabil@gmail.com>
2025-08-23 19:14:25 +0600

    Tag helpers- asp-* plu jQuery with fallbacks

--- CRUDSolution/CRUDExample/Controllers/PersonsController.cs
+++ CRUDSolution/CRUDExample/Controllers/PersonsController.cs
@@ -1,23 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ServiceContracts;
 using ServiceContracts.DTO;
 using ServiceContracts.Enums;
 using System.Security;
+using System.Threading.Tasks;
 
 namespace CRUDExample.Controllers
 {
+    [Route("[controller]")]
     public class PersonsController : Controller
     {
 
         private readonly IPersonsService _personsService;
+        private readonly ICountriesService _countriesService;
 
-        public PersonsController(IPersonsService personsService)
+        public PersonsController(IPersonsService personsService,ICountriesService countriesService)
         {
             _personsService = personsService;
+            _countriesService = countriesService;
         }
 
 
-        [Route("persons/index")]
+        [Route("[action]")] // Takes persons/index 
+        // [Route("/index")] // "/" override.iT takes /index
         [Route("/")]
         public async Task<IActionResult> Index(string searchBy,string? searchString,
             string sortBy=nameof(PersonResponse.PersonName) , 
@@ -46,6 +52,45 @@ namespace CRUDExample.Controllers
             List<PersonResponse> sortedPersons = await _personsService.GetSortedPersons(persons, sortBy, sortOrder);
 
             return View(sortedPersons);
+        }
+
+
+        [Route("[action]")]
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            List<CountryResponse>? countries =
+                await _countriesService.GetAllCountries();
+
+            ViewBag.Countries = countries.Select(temp =>
+            new SelectListItem()
+            {
+                Text=temp.CountryName,Value=temp.CountryID.ToString()
+            });
+            
+
+            return View();
+        }
+
+        [Route("[action]")]
+        [HttpPost]
+        public async Task<IActionResult> Create(PersonAddRequest personAddRequest)
+        {
+            if(!ModelState.IsValid)
+            {
+                List<CountryResponse>? countries =
+                await _countriesService.GetAllCountries();
+
+                ViewBag.Countries = countries;
+
+                ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+                ViewBag.PersonName = personAddRequest.PersonName;
+
+                return View();
+            }
+
+            PersonResponse personResponse= await _personsService.AddPerson(personAddRequest);
+            return RedirectToAction("Index", "Persons");
         }
     }
 }
